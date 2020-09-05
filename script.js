1317c14a725c8ae6a39e2a14a0d2f778fb5f8c09
Glitch (hello-webpage) <none>
2020-09-05 16:56:00 +0000

    🐟🏡 Checkpoint ./script.js:1605207/215 ./index.html:1605207/868 ./scratch.md:1605207/98

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-var h1 = document.getElementsByTagName('h1')[0].innerText;
-console.log(h1.toString());
+var h1 = document.getElementsByTagName('h1')[0];
+h1.addEventListener('click', function (event) {
+  alert('Hello world!');
+});
