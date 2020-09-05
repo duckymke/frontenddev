2f68365e4ae8a4c1ba04d8d3415b580ebdf48296
Glitch (hello-webpage) <none>
2020-09-05 16:44:05 +0000

    🏪📺 Checkpoint ./index.html:1605207/927 ./script.js:1605207/271 ./style.css:1605207/128

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+var h1 = document.getElementsByTagName('h1')[0].innerText;
+console.log(h1.toString());
